e62b057e90cb38b10f663f8992cf4a3cdf8711bc
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-18 15:03:02 +0600

    add readme file

---
+++ src/Routes/pages/Login/Login.jsx
@@ -0,0 +1,38 @@
+
+
+const Login = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="w-1/2 mr-8">
+                    <img className="" src="https://i.ibb.co/82QdkVX/login-Photo.png" alt="" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h2 className="text-2xl text-center font-bold">Please Login</h2>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
