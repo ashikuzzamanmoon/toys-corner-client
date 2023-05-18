3a159dedcd7f722588ea498781051d1a034024ce
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-18 16:45:32 +0600

    providers and authentication

--- src/Routes/pages/SignUp/SignUp.jsx
+++ src/Routes/pages/SignUp/SignUp.jsx
@@ -2,23 +2,25 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 
+const SignUp = () => {
 
-const Login = () => {
+    const { createUser } = useContext(AuthContext);
 
-    const {signIn} = useContext(AuthContext);
-    const handleLogin = event => {
+    const handleSignUp = event => {
         event.preventDefault();
         const form = event.target;
+        const name = form.name.value;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email, password);
+        const photo = form.photo.value;
+        console.log(name, email, password, photo);
 
-        signIn(email, password)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error => console.log(error));
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => console.log(error))
     }
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -29,7 +31,13 @@ const Login = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h2 className="text-2xl text-center font-bold">Please Login</h2>
-                        <form onSubmit={handleLogin}>
+                        <form onSubmit={handleSignUp}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="name" className="input input-bordered" />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -42,12 +50,18 @@ const Login = () => {
                                 </label>
                                 <input type="password" name="password" placeholder="password" className="input input-bordered" />
                             </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Photo URL</span>
+                                </label>
+                                <input type="text" name="photo" placeholder="photo url" className="input input-bordered" />
+                            </div>
                             <div className="form-control mt-6">
-                                <input className="btn btn-secondary" type="submit" value="Login" />
+                                <input className="btn btn-secondary" type="submit" value="Sign Up" />
                             </div>
                         </form>
                         <div className='text-center'>
-                            <p className='text-center fw-semibold mt-4'> Or Sign in with</p>
+                            <p className='text-center fw-semibold mt-4'> Or Sign up with</p>
                             <hr className="my-3" />
                         </div>
                         <div className='flex justify-center items-center'>
@@ -56,7 +70,7 @@ const Login = () => {
                             </div>
                         </div>
                         <div className='text-center mt-3'>
-                            <p className='font-semibold'>Do not have an account?<Link className="text-secondary" to="/signup"> Please Register</Link> </p>
+                            <p className='font-semibold'>Already have an account?<Link className="text-secondary" to="/login"> Please Login</Link> </p>
                         </div>
                     </div>
                 </div>
@@ -65,4 +79,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
