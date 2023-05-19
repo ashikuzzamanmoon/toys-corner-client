441d64a7c293833d72e5b717d8bfc56bfdde6da6
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-19 12:41:01 +0600

    gallery part all done

--- src/pages/Gallery/GalleryCard.jsx
+++ src/pages/Gallery/GalleryCard.jsx
@@ -1,7 +1,10 @@
 import { motion } from 'framer-motion';
+import Rating from 'react-rating';
+import { FaRegStar, FaStar } from 'react-icons/fa';
+import { BiLike }from 'react-icons/bi'
 
 const GalleryCard = ({ toy }) => {
-    const { img, name, price, ratings, likes } = toy;
+    const { img, name, price, ratings, likes, old_price } = toy;
     return (
         <motion.div
             className="bg-white rounded-lg shadow-md p-4"
@@ -9,14 +12,25 @@ const GalleryCard = ({ toy }) => {
             whileTap={{ scale: 0.95 }}
         >
             <img className="w-full h-[550px] rounded-lg" src={img} alt="" />
-            <div className="mt-2 mb-7 flex justify-between">
+            <div className="mt-2 mb-7 flex justify-between items-center">
                 <div>
-                    <h2 className="text-2xl font-semibold">{name} <div className="badge badge-secondary">NEW</div></h2>
+                    <p className="text-gray-600 mt-1 font-semibold text-xl line-through">Price: {old_price}</p>
                     <p className="text-gray-600 mt-1 font-semibold text-xl">Price: {price}</p>
                 </div>
                 <div>
-                    <p>{likes}</p>
-                    <p>{ratings}</p>
+                    <h2 className="text-3xl font-semibold">{name} <div className="badge badge-secondary">NEW</div></h2>
+                </div>
+                <div>
+                    <p className='text-gray-600 font-semibold text-xl inline-flex items-center gap-2'> <BiLike></BiLike>Likes: {likes}</p>
+                    <p className='text-gray-600 mt-1 font-semibold text-xl'>Ratings:
+                        <Rating
+                            placeholderRating={ratings}
+                            readonly
+                            emptySymbol={<FaRegStar></FaRegStar>}
+                            placeholderSymbol={<FaStar></FaStar>}
+                            fullSymbol={<FaStar></FaStar>}
+                        ></Rating>
+                    </p>
                 </div>
             </div>
         </motion.div>
